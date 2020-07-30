0078e331aeafcd525c1870f3e35504a12bceaa63
Vlad Holubiev <golubev.vld@gmail.com>
2020-07-30 16:07:42 +0300

    fix: try all in 1 line

--- publish.sh
+++ publish.sh
@@ -4,16 +4,7 @@ LIB_VERSION=3.1.1
 CHROMIUM_VERSION=83.0.4103.0
 LAYER_NAME='chrome-aws-lambda'
 
-ls -alh
-
-LAYER_VERSION=$(
-  aws lambda publish-layer-version --region "$TARGET_REGION" \
-    --layer-name $LAYER_NAME \
-    --zip-file="fileb://chrome_aws_lambda.zip" \
-    --description "chrome-aws-lambda v${LIB_VERSION} & Chromium v${CHROMIUM_VERSION}" \
-    --query Version \
-    --output text
-)
+LAYER_VERSION=$(aws lambda publish-layer-version --region "$TARGET_REGION" --layer-name $LAYER_NAME --zip-file="fileb://chrome_aws_lambda.zip" --description "chrome-aws-lambda v${LIB_VERSION} & Chromium v${CHROMIUM_VERSION}" --query Version --output text)
 
 aws lambda add-layer-version-permission \
   --region "$TARGET_REGION" \
