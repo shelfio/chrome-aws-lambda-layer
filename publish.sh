417d2ae510813cc3218748261482b42c6edbda74
Vlad Holubiev <golubev.vld@gmail.com>
2020-07-31 13:17:44 +0300

    feat: upload layer to s3 before publishing

--- publish.sh
+++ publish.sh
@@ -3,15 +3,25 @@
 LIB_VERSION=3.1.1
 CHROMIUM_VERSION=83.0.4103.0
 LAYER_NAME='chrome-aws-lambda'
+LAYER_DESCRIPTION="chrome-aws-lambda v${LIB_VERSION} & Chromium v${CHROMIUM_VERSION}"
+S3_BUCKET_NAME=shelf-lambda-layers-"$TARGET_REGION"
 
-LAYER_VERSION=$(aws lambda publish-layer-version --region "$TARGET_REGION" --layer-name $LAYER_NAME --zip-file="fileb://chrome_aws_lambda.zip" --description "chrome-aws-lambda v${LIB_VERSION} & Chromium v${CHROMIUM_VERSION}" --query Version --output text)
+aws s3 cp \
+  ./chrome_aws_lambda.zip \
+  s3://"$S3_BUCKET_NAME"/chrome_aws_lambda.zip
 
 aws lambda add-layer-version-permission \
   --region "$TARGET_REGION" \
-  --layer-name $LAYER_NAME \
+  --layer-name "$LAYER_NAME" \
   --statement-id sid1 \
   --action lambda:GetLayerVersion \
   --principal '*' \
-  --query Statement \
-  --output text \
-  --version-number "$LAYER_VERSION"
+  --version-number "$(
+    aws lambda publish-layer-version \
+      --region "$TARGET_REGION" \
+      --layer-name "$LAYER_NAME" \
+      --description "$LAYER_DESCRIPTION" \
+      --query Version \
+      --output text \
+      --content S3Bucket="$S3_BUCKET_NAME",S3Key="$LAYER_FILENAME"
+  )"
